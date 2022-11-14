5c3ac85222755fbe57404cb3a2a108234ead238b
bogusheva <110731443+bogusheva@users.noreply.github.com>
2022-11-14 14:23:02 +0200

    Initial commit

---
+++ src/js/index.js
@@ -0,0 +1,9 @@
+const burger = document.querySelector(".js-burger-btn");
+const header = document.querySelector(".js-header");
+const searchBtn = document.querySelector(".js-search-btn");
+
+burger.addEventListener("click", () => {
+  header.classList.toggle("active");
+});
+
+searchBtn.addEventListener("click", () => {});
